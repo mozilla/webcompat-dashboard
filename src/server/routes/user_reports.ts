e497b20c9557fe51be8d34e33e31713f9c89d56d
Ted Campbell <tcampbell@mozilla.com>
2024-04-11 15:26:13 -0400

    Re-use Worker for /user_reports to avoid parsing overhead
    Reuse the Worker between requests so we don't have to load server
    sources each time. Startup time can be serveral seconds otherwise.
    For simplicity, avoid worrying about worker clusters and just use
    a single worker for now.

--- src/server/routes/user_reports.ts
+++ src/server/routes/user_reports.ts
@@ -1,10 +1,16 @@
-import { Worker } from "node:worker_threads";
+import { Worker, MessageChannel } from "node:worker_threads";
 import * as path from "node:path";
 import { Request, Response } from "express";
 import type { Logger } from "winston";
-import { getDefaultProject } from "../helpers/bigquery";
 
 import { endWithStatusAndBody, getParsedUrl } from "../helpers/http";
+
+// Create a separate Worker thread to do the requests on since the
+// data processing is CPU intensive and would otherwise interfere
+// with mainthread responsiveness.
+// For now, only a single worker is used though multiple BQ requests
+// can be in-flight on it.
+const worker = new Worker(path.join(__dirname, "..", "helpers", "user_reports_transform.ts"));
 
 export default async function handleUserReports(logger: Logger, req: Request, res: Response) {
   const childLogger = logger.child({ handler: "handleUserReports" });
@@ -16,16 +22,21 @@ export default async function handleUserReports(logger: Logger, req: Request, re
   }
 
   try {
-    const worker = new Worker(path.join(__dirname, "..", "helpers", "user_reports_transform.ts"), {
-      workerData: {
-        projectId: getDefaultProject(),
+    // Post the request to the existing worker, and use a new MessageChannel
+    // to ensure we only see our own results even if other requests are in-flight.
+    const { port1, port2 } = new MessageChannel();
+    worker.postMessage(
+      {
+        type: "fetch",
+        projectId: process.env.BQ_PROJECT_ID,
         paramFrom: searchParams.get("from")!,
         paramTo: searchParams.get("to")!,
+        port: port1,
       },
-    });
-
+      [port1],
+    );
     const results = await new Promise((resolve) => {
-      worker.on("message", (msg) => {
+      port2.on("message", (msg) => {
         if (msg.type == "done") {
           resolve(msg.result);
         } else if (msg.type == "verbose") {
